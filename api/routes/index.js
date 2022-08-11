84faf0ba3372b94fb4c5d4ef703fc26f1b618500
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-11 17:23:47 +0100

    Routers created

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,3 +1,10 @@
 const router = require ('express').Router()
 
+const authRouter = require('./auth.router')
+const userRouter = require('./user.router')
+const movieRouter = require('./movie.router')
+const actorRouter = require('./actor.router')
+const directorRouter = require('./director.router')
+const genreRouter = require('./genre.router')
+
 module.exports = router
---
+++ api/routes/user.router.js
@@ -0,0 +1,7 @@
+const router = require ('express').Router()
+
+// router
+//   .post('/signup', signup)
+//   .post('/login', login)
+
+module.exports = router
