260d950f754dff3437c32bb21340d2660eeaa46a
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-16 17:08:42 +0100

    actor controller modified - able to add movies to an actor

--- api/controllers/actor.controller.js
+++ api/controllers/actor.controller.js
@@ -3,8 +3,9 @@ const Actor = require ('../models/actor.model')
 async function createActor (req, res) {
   try {
     const actor = await Actor.create({
-      name: req.body.name
+      name: req.body.name,
     })
+    actor.addMovies([req.body.movieId])
     res.status(200).json({ message: 'Actor created', actor: actor })
   } catch (error) {
     res.status(500).send(error.message)
