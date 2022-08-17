9a995be3f35ce3369d538958dbe13037c10b821e
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-17 10:09:14 +0100

    CRUD for movies done

--- api/controllers/movie.controller.js
+++ api/controllers/movie.controller.js
@@ -1,14 +1,75 @@
 const Movie = require ('../models/movie.model')
+const { Op } = require('sequelize')
+
+async function getAllMovies (req, res) {
+  try {
+    if (!Object.values(req.query).length) {
+      const movies = await Movie.findAll()
+      return res.status(200).json(movies)
+    } else {
+      const movies = await Movie.findAll({
+        where: {
+          [Op.and]: [
+            req.query
+          ]
+        }
+      })
+      return !movies ? res.status(404).send('No matchs found') : res.status(200).json(movies)
+    }
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function getOneMovie (req, res) {
+  try {
+    const movie = await Movie.findByPk(req.params.id)
+    return !movie ? res.status(404).send('Movie not found') : res.status(200).json(movie)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
 
 async function createMovie (req, res) {
   try {
     const movie = await Movie.create(req.body)
-    res.status(200).json({ message: 'Movie created', movie: movie })
+    return res.status(200).json({ message: 'Movie created', movie: movie })
   } catch (error) {
-    res.status(500).send(error.message)
+    return res.status(500).send(error.message)
+  }
+}
+
+async function updateMovie (req, res) {
+  try {
+    const movie = await Movie.update(req.body, {
+      returning: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    return !movie[1].length ? res.status(404).send('Movie not found') : res.status(200).json({ message: 'Movie updated', movie: movie[1] })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function deleteMovie (req, res) {
+  try {
+    const movie = await Movie.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    return !movie ? res.status(404).send('Movie not found') : res.status(200).json('Movie deleted')
+  } catch (error) {
+    return res.status(500).send(error.message)
   }
 }
 
 module.exports = {
-  createMovie
+  getAllMovies,
+  getOneMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie
 }
--- api/routes/movie.router.js
+++ api/routes/movie.router.js
@@ -1,10 +1,18 @@
 const router = require ('express').Router()
 
 const {
-  createMovie
+  getAllMovies,
+  getOneMovie,
+  createMovie,
+  updateMovie,
+  deleteMovie
 } = require('../controllers/movie.controller')
 
 router
+  .get('/', getAllMovies)
+  .get('/:id', getOneMovie)
   .post('/', createMovie)
+  .put('/:id', updateMovie)
+  .delete('/:id', deleteMovie)
 
 module.exports = router
