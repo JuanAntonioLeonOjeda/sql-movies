a6cf1733f5db850d62fdc7fec949c5e32f72e5b4
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-17 13:53:33 +0100

    Awards CRUD done

--- api/controllers/awards.controller.js
+++ api/controllers/awards.controller.js
@@ -1,5 +1,23 @@
 const Awards = require ('../models/awards.model')
 
+async function getAllAwards (req, res) {
+  try {
+    const awards = await Awards.findAll()
+    return res.status(200).json(awards)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function getOneAward (req, res) {
+  try {
+    const award = await Awards.findByPk(req.params.id)
+    return !award ? res.status(404).send('Awards code not found') : res.status(200).json(award)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 async function createAwards (req, res) {
   try {
     const awards = await Awards.create(req.body)
@@ -9,6 +27,37 @@ async function createAwards (req, res) {
   }
 }
 
+async function updateAwards (req, res) {
+  try {
+    const awards = await Awards.update(req.body, {
+      returning: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    return !awards[1].length ? res.status(404).send('Awards code not found') : res.status(200).json({ message: 'Awards updated', awards: awards[1] })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function deleteAwards (req, res) {
+  try {
+    const awards = await Awards.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    return !awards ? res.status(404).send('Awards code not found') : res.status(200).send('Awards deleted')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createAwards
+  getAllAwards,
+  getOneAward,
+  createAwards,
+  updateAwards,
+  deleteAwards
 }
--- api/routes/awards.router.js
+++ api/routes/awards.router.js
@@ -1,10 +1,18 @@
 const router = require ('express').Router()
 
 const {
-  createAwards
+  getAllAwards,
+  getOneAward,
+  createAwards,
+  updateAwards,
+  deleteAwards
 } = require ('../controllers/awards.controller')
 
 router
+  .get('/', getAllAwards)
+  .get('/:id', getOneAward)
   .post('/', createAwards)
+  .put('/:id', updateAwards)
+  .delete('/:id', deleteAwards)
 
 module.exports = router
