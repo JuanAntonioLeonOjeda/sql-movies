1c8185857cc6d02f1bd677b2e7e0951a5d3eab1d
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-17 15:14:24 +0100

    movie controller - assignAwards function modified

--- api/controllers/movie.controller.js
+++ api/controllers/movie.controller.js
@@ -64,7 +64,7 @@ async function assignAwards (req, res) {
     if ( !awards ) {
       return res.status(404).send('Awards code not found')
     }
-    await movie.setAwards(awards)
+    await movie.setAward(awards)
     return res.status(200).json({ message: 'Awards assigned to movie: ' + movie.title, movie: movie })
   } catch (error) {
     return res.status(500).send(error.message)
