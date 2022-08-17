e111f68a09e4632cadefb128234532c507765c6c
root <root@LAPTOP-LMJQULD0.localdomain>
2022-08-17 10:47:21 +0100

    Genre CRUD created

--- api/controllers/genre.controller.js
+++ api/controllers/genre.controller.js
@@ -1,5 +1,23 @@
 const Genre = require('../models/genre.model')
 
+async function getAllGenres (req, res) {
+  try {
+    const genres = await Genre.findAll()
+    return res.status(200).json(genres)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function getOneGenre (req, res) {
+  try {
+    const genre = await Genre.findByPk(req.params.id)
+    return !genre ? res.status(404).send('Genre not found') : res.status(200).json(genre)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 async function createGenre (req, res) {
   try {
     const genre = await Genre.create({
@@ -11,6 +29,37 @@ async function createGenre (req, res) {
   }
 }
 
+async function updateGenre (req, res) {
+  try {
+    const genre = await Genre.update(req.body, {
+      returning: true,
+      where: {
+        id: req.params.id
+      }
+    })
+    return !genre[1].length ? res.status(404).send('Genre not found') : res.status(200).json({ message: 'Genre updated', genre: genre[1] })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+async function deleteGenre (req, res) {
+  try {
+    const genre = await Genre.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    return !genre ? res.status(404).send('Genre not found') : res.status(200).json('Genre deleted')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  createGenre
+  getAllGenres,
+  getOneGenre,
+  createGenre,
+  updateGenre,
+  deleteGenre
 }
--- api/routes/genre.router.js
+++ api/routes/genre.router.js
@@ -1,10 +1,18 @@
 const router = require ('express').Router()
 
 const {
-  createGenre
+  getAllGenres,
+  getOneGenre,
+  createGenre,
+  updateGenre,
+  deleteGenre
 } = require ('../controllers/genre.controller')
 
 router
+  .get('/', getAllGenres)
+  .get('/:id', getOneGenre)
   .post('/', createGenre)
+  .put('/:id', updateGenre)
+  .delete('/:id', deleteGenre)
 
 module.exports = router
